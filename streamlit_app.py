345c598712cee63d3f6e17d1b417fddf81755074
ivaanaw <vanaivy79@gmail.com>
2025-05-27 09:41:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,14 @@ st.title("Masha gemas")
 st.write(
     "Sayangi Masha anak gemas imut cantik manis"
 )
+st.image("IMG_7996.jpeg", width=200)
+
+st. tittle("Aplikasi Sederhana")
+st. header ("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input ("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
+    
